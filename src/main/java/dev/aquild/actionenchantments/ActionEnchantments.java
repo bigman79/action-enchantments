31477101d0c7dc63ab3d6fcad971446efe2ca097
Max Niederman <maxniederman@gmail.com>
2020-06-28 18:55:49 -0700

    Added attack listener

--- src/main/java/dev/aquild/actionenchantments/ActionEnchantments.java
+++ src/main/java/dev/aquild/actionenchantments/ActionEnchantments.java
@@ -1,5 +1,6 @@
 package dev.aquild.actionenchantments;
 
+import dev.aquild.actionenchantments.Listeners.Attack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ActionEnchantments extends JavaPlugin {
@@ -9,7 +10,10 @@ public final class ActionEnchantments extends JavaPlugin {
         // Config
         this.saveDefaultConfig();
 
-        this.getLogger().info("Action Enchantments by Aquild");
+        // Attach listeners
+        this.getServer().getPluginManager().registerEvents(new Attack(this), this);
+
+        this.getLogger().info("Loaded Action Enchantments by Aquild");
     }
 
     @Override
--- src/main/java/dev/aquild/actionenchantments/Listeners/Attack.java
+++ src/main/java/dev/aquild/actionenchantments/Listeners/Attack.java
@@ -1,7 +1,38 @@
 package dev.aquild.actionenchantments.Listeners;
 
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class Attack implements Listener {
+    private final JavaPlugin plugin;
+    private final HashMap<UUID, Integer> players;
 
+    public Attack(JavaPlugin plugin) {
+        this.plugin = plugin;
+        this.players = new HashMap<UUID, Integer>();
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageByEntityEvent event) {
+        Player player = (Player) event.getDamager();
+
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (EnchantmentTarget.WEAPON.includes(item)) {
+            this.players.put(player.getUniqueId(), this.players.getOrDefault(player.getUniqueId(), 0) + 1);
+
+            if (this.players.get(player.getUniqueId()) == 3) {
+                this.players.put(player.getUniqueId(), 0);
+                item.addUnsafeEnchantment(Enchantment.DAMAGE_ALL, item.getEnchantmentLevel(Enchantment.DAMAGE_ALL) + 1);
+            }
+        }
+    }
 }
